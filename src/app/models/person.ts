d336e786772977632ba89fbdebc5611f1279452b
seiit <cristian.gomez@rokk3rlabs.com>
2019-01-25 15:43:47 -0500

    change ver

--- src/app/models/person.ts
+++ src/app/models/person.ts
@@ -1,12 +1,14 @@
 export class Person {
-    constructor(_id = null,nombre = "",apellido = "", correo = ""){
+    constructor(_id = null,nombre = "",apellido = "", correo = "",password = ''){
         this._id= _id;
         this.name = nombre;
         this.lastname = apellido;
         this.email = correo;
+        this.password = password;
     }
     _id: string;
     name: string;
     lastname: string;
     email: string;
+    password : string;
 }
