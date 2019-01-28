79c40820e012865474760b786e63fc9e4f62b919
seiit <cristian.gomez@rokk3rlabs.com>
2019-01-28 09:48:30 -0500

    relase complete

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,14 +1,14 @@
 
 export class Config {
-  constructor(server = 'http://localhost:3000'){
+  /*constructor(server = 'http://localhost:3000'){
   this.server = server;
   }
   server : string;
   }
-
-  /*constructor(server = 'https://frontstaff.herokuapp.com/'){
+*/
+  constructor(server = 'https://frontstaff.herokuapp.com/'){
     this.server = server;
     }
     server : string;
     }
-    */
+    
